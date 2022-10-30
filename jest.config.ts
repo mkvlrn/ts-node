636b64a47d0a25ebc2c9bbb409582ec93e816406
Mike Valeriano <mkvlrn@gmail.com>
2022-10-30 14:05:41 -0300

    chore: adjust some config files end dev deps

--- jest.config.ts
+++ jest.config.ts
@@ -7,12 +7,14 @@ const config: Config = {
   passWithNoTests: true,
   preset: 'ts-jest',
   rootDir: './',
-  testRegex: '.(spec|test).(ts|tsx)$',
-  testEnvironment: 'node',
+  testRegex: '.spec.(ts|tsx)$', // "spec" for unit tests, "test" for integration or e2e
+  testEnvironment: 'node', // "jsdom" for react
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>',
   }),
-  collectCoverageFrom: ['./src/**/*.ts', '!./src/index.ts'],
+  transform: {
+    '^.+.(png|svg|jpg|gif|webp)$': 'jest-transform-stub',
+  },
 };
 
 export default config;
