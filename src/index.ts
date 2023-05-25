bd7d190cfaae22661d92f8af030d5358851987cb
Mike Valeriano <mkvlrn@gmail.com>
2023-05-24 23:16:05 -0300

    chore: use shared tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { AdvancedMath } from '#/supermath/advanced-math.js';
+import { multiply } from '#/supermath/advanced-math.js';
 
-const math = new AdvancedMath();
-
-const result = math.multiply(210, 2);
+const result = multiply(210, 2);
 
 console.log(`210 x 2 = ${result}`);
