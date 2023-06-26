2da3fe55dc2bc97a35f884fc32bd96756ac11328
Mike Valeriano <mkvlrn@gmail.com>
2023-06-25 22:03:07 -0300

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { resolve } from 'path';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   resolve: {
-    alias: { '#': resolve(__dirname, './src') },
+    alias: { '#': resolve('.', './src') },
   },
   test: {
     coverage: {
